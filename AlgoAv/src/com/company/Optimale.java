0f5264422f44898d58824a1192471a61ccc57607
Moncef18 <mrejebsf@enssat.fr>
2019-05-02 14:27:33 +0200

    commentaires

--- AlgoAv/src/com/company/Optimale.java
+++ AlgoAv/src/com/company/Optimale.java
@@ -23,7 +23,7 @@ public class Optimale {
 		double min=1;
 		double max=n;
 
-	    for(int i=1; i<=8; i++) {
+	    for(int i=1; i<=n; i++) {
 			double x = (int) (Math.random() * ((max - min) + 1)) + min;
 
 			points.add(new Point(i,x));
@@ -64,7 +64,7 @@ public class Optimale {
 		return result;
 	}*/
 	
-	public void solution_2(int i, int last, double somme){
+	public void solution_Essais_successifs(int i, int last, double somme){
 		
 		// Etape finale:
 		if(i == size-1){
@@ -73,12 +73,12 @@ public class Optimale {
 		}
 		
 		// Etape 1:
-		solution_elagage(i+1, last, somme);
+		solution_Essais_successifs(i+1, last, somme);
 		
 		// Etape 2:
 		double check = 0;
 		check = somme + distance(last, i) + Cout;
-		solution_2(i+1, i, check);
+		solution_Essais_successifs(i+1, i, check);
 	
 	}
 	
@@ -132,8 +132,10 @@ public class Optimale {
 	}
 	
 	public static void main(String args[]) {
-		int n =8;
+		int n =33;						      //on choisit le nombre de points qu'on veut (inutile si on utilise le Deuxieme constructeur
+		                                      //qui récupère un fichier de données
 		Optimale p = new Optimale(n);
+		//Optimale p = new Optimale();       // à décommenter si on veut introduire un jeu de test et non générer aléatoirement les pts
 		List<Point> l = new ArrayList<>();
 		double value = 0;
 		System.out.println("n = " + n);
@@ -146,35 +148,37 @@ public class Optimale {
 		*/
 		long startTime = System.currentTimeMillis();
 		O = Double.POSITIVE_INFINITY;
-		p.solution_2(1, 0, 0);
+		p.solution_Essais_successifs(1, 0, 0);
 		long stopTime = System.currentTimeMillis();
-		System.out.println("La solution optimale 2 ====> " + O + " Time : " + (stopTime - startTime) + " ms");
+		System.out.println("La solution sans élagage ==> score=" + O + " |Time : " + (stopTime - startTime) + " ms");
 		
 		startTime = System.currentTimeMillis();
 		O = Double.POSITIVE_INFINITY;
-		p.solution_elagage(1, 0, 0);
+	    p.solution_elagage(1, 0, 0);
 		stopTime = System.currentTimeMillis();
-		System.out.println("La solution avec elagage ==> " + O + " Time : " + (stopTime - startTime) + " ms");
+		System.out.println("La solution avec élagage ==> score= " + O + " |Time : " + (stopTime - startTime) + " ms");
 		
 		startTime = System.currentTimeMillis();
 		List<Double[]> T = p.approx_opt();
 		LinkedList<Point> set = p.backward(T);
 		stopTime = System.currentTimeMillis();
 		System.out.println("Backward sequence " + set.toString());
-		System.out.println("Programmatin dynamique ====> " + T.get(T.size()-1)[1]+ " Time : " + (stopTime - startTime) + " ms");
-		Set <Ligne> lignes = new HashSet<Ligne>();
-		Set<Point> setP = new HashSet<Point>(p.points);
+		System.out.println("Programmatin dynamique ====> score=" + T.get(T.size()-1)[1]+ " |Time : " + (stopTime - startTime) + " ms");
 
-		Set<Point> setF = new HashSet<Point>(set);
+
+		Set <Ligne> lignes = new HashSet<Ligne>();
+		Set<Point> setP = new HashSet<Point>(p.points); //ensemble de tous les points
+
+		Set<Point> setF = new HashSet<Point>(set);      //ensemble des points qui constituent la ligne brisée optimale
 		int i=0;
 		System.out.println(set.size());
-		while (i<set.size()-1){
+		while (i<set.size()-1){                     //boucle qui relie tous les points de la ligne brisée optimale pour les afficher
 		 lignes.add(new Ligne (set.get(i),set.get(i+1)));
 		 i++;
 
 
 		}
-		Visu v1 = new Visu(setP,lignes,"Solution de score "+T.get(T.size()-1)[1]);
+		Visu v1 = new Visu(setP,lignes,"Solution de score "+T.get(T.size()-1)[1]); //affichage de la solution optimale+ score optimal
 
 	}
 
